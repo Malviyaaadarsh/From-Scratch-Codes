661f2a9dd325085d8b43a0a0217719154a606891
Aadarsh Malviya <malviyaaadarsh45@gmail.com>
2026-08-16 00:57:18 +0530

    Nearest Centroid Classifier Implementation

--- ML_Algorithms/Nearest_Centroid_Classifier.py
+++ ML_Algorithms/Nearest_Centroid_Classifier.py
@@ -16,3 +16,14 @@ class NearestCentroid:
     def score(self,x,y):
         return np.mean(self.predict(x)==y)
 
+
+
+# sklearn implementation
+from sklearn.neighbors import NearestCentroid
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+X,y = make_classification(n_samples=1000, n_features=3, n_redundant=0, n_clusters_per_class=1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+model = NearestCentroid()
+model.fit(X_train,y_train)
+print(model.score(X_test,y_test))
